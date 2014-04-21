f16f5d2d061a4d9a39be950cf1800acf8e0ae642
vedopar <vedopar.s@gmail.com>
2014-04-20 21:09:48 -0700

    use http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8&q= , no success because cannot search image from blob

--- image0search.py
+++ image0search.py
@@ -4,18 +4,35 @@ Created on 2014-4-18
 @author: vedopar
 '''
 import os
-import urllib
-import webapp2
+import webapp2,json
+import urllib,urllib2,re
+import cookielib
 
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.api import images
 
+def google_search2(req):
+    url='https://ajax.googleapis.com/ajax/services/search/images'+'?v=1.0&rsz=8&q='+req
+    #url = ('https://ajax.googleapis.com/ajax/services/search/images?' +'v=1.0&q=barack%20obama&userip=INSERT-USER-IP')
+
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    json_acceptable_string = response.read().replace("'", "\"")
+    d = json.loads(json_acceptable_string)
+    res=""
+    for r in d['responseData']['results']:
+        res=res+r['titleNoFormatting'].encode('utf-8')+' '
+        res=res+r['contentNoFormatting'].encode('utf-8')+'\n'
+    return res
+        
+        
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         for key in blobstore.BlobInfo.all().run():
             key.delete()
         upload_url = blobstore.create_upload_url('/upload')
-        self.response.out.write('<html><body>')
+        #google_search()
         self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
         self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit" name="submit" value="Submit"> </form></body></html>""")
 
@@ -29,7 +46,11 @@ class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self, resource):
         resource = str(urllib.unquote(resource))
         blob_info = blobstore.BlobInfo.get(resource)
-        self.send_blob(blob_info)
+        #self.send_blob(blob_info)
+        url= images.get_serving_url(blob_info.key())
+        self.response.out.write("<html><body><p>"+url+"</p>")
+        self.response.out.write(google_search2(url))
+        self.response.out.write("</p></body></html>")
 
 application = webapp2.WSGIApplication([('/', MainHandler),
                                ('/upload', UploadHandler),
